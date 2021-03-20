ed4260e42bd6e4aba2078a55de375836a12adcfe
Elham Hasanpour <elhamhassanpour133@gmail.com>
2021-03-20 11:48:20 +0100

    Upload multi image API `

---
+++ Upload/Controller/Upload.js
@@ -0,0 +1,22 @@
+const multer= require('multer');
+const path =require('path');
+// const util = require('util');
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,'./uploads');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+ exports.upload = multer({ storage: storage, fileFilter: fileFilter }).array("multi-image");
---
+++ Upload/Route/upload.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { upload } = require('../Controller/Upload');
+
+const router = express();
+
+router.post('/upload',
+    upload);
+
+
+module.exports = router;
