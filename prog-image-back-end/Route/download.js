538828dfbef909710a1aeaea142b101273f2b2c0
Elham Hasanpour <elhamhassanpour133@gmail.com>
2021-04-21 17:49:20 +0200

    delete directory prog-image

--- prog-image-back-end/Route/download.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { download } = require('../Controller/download');
-
-const router = express();
-
-router.get('/download/:_id',
-   download);
-
-module.exports=router;    
